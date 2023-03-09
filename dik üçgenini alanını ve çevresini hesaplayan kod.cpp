2f036f15fc7f63763a0da08e89b8f20233b0d4a6
conqueror1071 <127208356+conqueror1071@users.noreply.github.com>
2023-03-09 16:22:25 +0300

    C++ Çalışması 3.2
    Dik üçgende hipotenüsü, üçgenin çevresini ve alanını hesaplayan kod çalışması

---
+++ dik üçgenini alanını ve çevresini hesaplayan kod.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <clocale>
+#include <math.h>
+//üçgenin çevresi tüm kenarların toplamıdır
+//üçgenin alanı= taban.yükseklik/2
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "Turkish");
+
+	float taban, dik;
+	
+	cout << "Üçgenin tabanının uzunluğunu birim cinsinden yazınız = ";
+	cin >> taban;
+	cout << "Üçgenin tabana dik kenarının uzunluğunu birim cinsinden yazınız = ";
+	cin >> dik;
+
+	float hipotenus = sqrt(pow(taban, 2) + pow(dik, 2));
+	float alan = taban * dik / 2;
+	cout << "Üçgenin hipotenüsünün uzunluğu = " << hipotenus << endl;
+	cout << "Üçgenin çevresi = " << taban + dik + hipotenus <<" birimdir "<< endl;
+	cout << "Üçgenin alanı = " << alan <<" birim karedir "<< endl;
+	return 0;
+}
