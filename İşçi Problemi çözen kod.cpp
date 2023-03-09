e1afd27bcccc3954e528dc71f6e61d1e02855c58
conqueror1071 <127208356+conqueror1071@users.noreply.github.com>
2023-03-09 15:46:32 +0300

    C++ Çalışması 3
    Basit düzeyde işçi problemi çözen kod çalışması

---
+++ İşçi Problemi çözen kod.cpp
@@ -0,0 +1,15 @@
+﻿#include<iostream>
+#include <clocale>
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "Turkish");
+	int gün,ıscı;
+	cout << "1 işçi işi kaç günde bitirebiliyor?" << endl;
+	cin >> gün;
+	cout << "Toplam kaç işçi çalışacak?" << endl;
+	cin >> ıscı;
+	cout << "İşin bitme süresi = " << (float)gün/ıscı<< " gün";
+	return 0;
+
+}
