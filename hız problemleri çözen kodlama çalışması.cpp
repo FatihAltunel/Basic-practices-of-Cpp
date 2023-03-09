2a4e1045fc7b20f6dce40deb9c30a38c08d46e37
conqueror1071 <127208356+conqueror1071@users.noreply.github.com>
2023-03-09 17:51:18 +0300

    C++Çalışması 3.4
    Basit düzeyde hız problemini çözüp varış süresini saat, dakika ve saniye cinsinden ölçen kodlama çalışması

---
+++ hız problemleri çözen kodlama çalışması.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <clocale>
+#include <math.h>
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "Turkish");
+	int mesafe;
+	float surat;
+	cout << "Lütfen aradaki mesafeyi km cinsinden giriniz = ";
+	cin >> mesafe;
+	cout << "Lütfen mesafeyi alacağınız sürati km/h cinsinden giriniz = ";
+	cin >> surat;
+	int saat = mesafe / surat;
+	float dakika = (float)mesafe / surat - saat;
+	float dakika2 = dakika * 60; //12.2 dakika
+	int dakika3 = dakika * 60;
+	float saniye = dakika2 - dakika3;
+	int saniye2 = saniye * 60;
+	cout << "Tahmini varış süreniz = " << saat << " Saat " << dakika3 << " Dakika " << saniye2 << " Saniyedir" << endl;
+
+	return 0;
+}
