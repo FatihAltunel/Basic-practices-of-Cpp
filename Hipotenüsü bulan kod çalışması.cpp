eabac210e8e8ba3ed2e7a0a5785e3f53e70460aa
conqueror1071 <127208356+conqueror1071@users.noreply.github.com>
2023-03-09 16:03:25 +0300

    C++ Çalışması 3.1
    Dik üçgende hipotenüsü bulan kod çalışması

---
+++ Hipotenüsü bulan kod çalışması.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <clocale>
+#include <math.h>
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "Turkish");
+	int taban, dik;
+	cout << "Üçgenin tabanının uzunluğunu yazınız = ";
+	cin >> taban;
+	cout << "Üçgenin tabana dik kenarının uzunluğunu yazınız = ";
+	cin >> dik;
+	cout << "Hipotenüsün uzunluğu = " << sqrt(pow(taban, 2) + pow(dik, 2)) << endl;
+	return 0;
+}
