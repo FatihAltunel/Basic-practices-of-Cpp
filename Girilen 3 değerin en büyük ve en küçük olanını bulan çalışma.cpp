0b3be648cae36c71117b07e0bbc7577983a84f82
conqueror1071 <127208356+conqueror1071@users.noreply.github.com>
2023-03-09 14:58:42 +0300

    C++ Çalışması 2
    Girilen 3 değerin en büyük ve en küçük olanını bulan ve ekranda yazdıran çalışma

---
+++ Girilen 3 değerin en büyük ve en küçük olanını bulan çalışma.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <clocale>
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "Turkish");
+	int a, b, c;
+	cout << "Lütfen ekrana bir değer giriniz = ";
+	cin >> a;
+	cout << "Lütfen ekrana ikinci değeri giriniz = ";
+	cin >> b;
+	cout << "Lütfen ekrana üçüncü değeri giriniz = ";
+	cin >> c;
+	cout << "Girdiğiniz değerler = " << a << "," << b << "," << c << endl;
+
+	if (a > b && b > c) {
+		cout << "En büyük değer = " << a << endl;
+		cout << "En küçük değer = " << c << endl;
+	}
+	else if (a > b && a > c && b < c) {
+		cout << "En büyük değer = " << a << endl;
+		cout << "En küçük değer = " << b << endl;
+	}
+	else if (b > a && a > c) {
+		cout << "En büyük değer = " << b << endl;
+		cout << "En küçük değer = " << c << endl;
+	}
+	else if (b > a && b > c && c > a) {
+		cout << "En büyük değer = " << b << endl;
+		cout << "En küçük değer = " << a << endl;
+	}
+	else if (c > a && a > b) {
+		cout << "En büyük değer = " << c << endl;
+		cout << "En küçük değer = " << b << endl;
+	}
+	else if (c > a && c > b && a < b) {
+		cout << "En büyük değer = " << c << endl;
+		cout << "En küçük değer = " << a << endl;
+	}
+
+}
