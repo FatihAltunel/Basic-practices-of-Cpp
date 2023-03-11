0fe8808caf467d84e99bffcddfdcd7095caa6c2d
conqueror1071 <127208356+conqueror1071@users.noreply.github.com>
2023-03-11 13:56:54 +0300

    Update Cokkatlidongu.cpp

--- Cokkatlidongu.cpp
+++ Cokkatlidongu.cpp
@@ -1,7 +1,10 @@
 ﻿#include <iostream>
 #include <clocale>
 #include<math.h>
-
+//1.Kolonda: 1'den 100'e kadar olan 15'in katları
+//2.Kolonda: 1'den 30'a kadar olan 5'in katları
+//3.Kolonda 100'den 50'ye kadar olan 10'un katları
+//4.Kolonda 2'den 64'e kadar olan 2'nin üstleri
 using namespace std;
 
 int main() {
