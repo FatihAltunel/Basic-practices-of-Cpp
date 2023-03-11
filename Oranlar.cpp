d4af91be824f67a9044d421c280baab883bfd0e0
conqueror1071 <127208356+conqueror1071@users.noreply.github.com>
2023-03-11 18:02:49 +0300

    C++ Çalışması 7
    Kullanıcının istediği kadar girdiği sayıların içerisindeki pozitif, negatif ve sıfırların oranını gösteren çalışma

---
+++ Oranlar.cpp
@@ -0,0 +1,30 @@
+﻿//kullanıcıdan bir sayı alıp verilen sayı kadar sayıyı okuyunuz
+//bu sayıların içerisindeki pozitif negatif ve sıfırların oranını döndürünüz
+#include <iostream>
+#include <clocale>
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "Turkish");
+	int deger1;
+	int pozitif = 0;
+	int negatif = 0;
+	int sıfır = 0;
+	cout << "Ekrana yazdırmak istediğinizi sayı adetini giriniz = ";
+	cin >> deger1;
+
+	for (int i = 1; i <= deger1; i++) {
+		int deger2;
+		cin >>deger2 ;
+		if (deger2 > 0)
+			pozitif++;
+		else if (deger2 < 0)
+			negatif++;
+		else {
+			sıfır++;
+		}		
+	}
+	cout << "Pozitifler = " << (float)pozitif / deger1 << endl; 
+	cout << "Negatifler = " << (float)negatif / deger1 << endl; 
+	cout << "Sıfırlar = " << (float)sıfır / deger1 << endl;
+}
