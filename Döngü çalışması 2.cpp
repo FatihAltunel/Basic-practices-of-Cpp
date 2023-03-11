ecc54b387bfcb0dd8469f924d12723429cab14e3
conqueror1071 <127208356+conqueror1071@users.noreply.github.com>
2023-03-11 12:06:01 +0300

    C++ Çalışması 4
    Kullanıcı -1 girene kadar girilen sayıların ortalamasını alan kodlama çalışması

---
+++ Döngü çalışması 2.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <clocale>
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "Turkish");
+	cout << "Lütfen ekrana istediğiniz kadar sayı giriniz ve daha sonra -1 yazınız" << endl;
+	int a{0};
+	int b = 0;
+	int c{0};
+	while (b != -1) {
+		cin >> b;
+		if (b == -1 && a==0) {
+			cout << "Hiç sayı girmediniz!!!" << endl;
+		}
+		else if (b == -1) {
+			cout << "Girdiğiniz sayıların ortalaması = " << (float)a / c;
+		}
+		else {
+			a += b;
+			c++;
+		}
+	}
+}
