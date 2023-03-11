41ac1b24fe45eedcbc1b20b09b524878d098d219
conqueror1071 <127208356+conqueror1071@users.noreply.github.com>
2023-03-11 13:56:26 +0300

    C++ Çalışması 5
    Birden fazla döngüyü kolonlar halinde ekrana yazdıran kodlama uygulaması

---
+++ Cokkatlidongu.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include <clocale>
+#include<math.h>
+
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "Turkish");
+	cout << "1.Kolon = ";
+	for (int i = 1; i <= 100; i++) {
+		if (i % 15 == 0) {
+			cout << i << " ";
+		}
+		if (i == 90) {
+			cout << endl;
+		}
+	}
+	cout << "2.Kolon = ";
+	for (int a = 1; a <= 30; a++) {
+		if (a % 5 == 0) {
+			cout << a << " ";
+		}
+		if (a == 30) {
+			cout << endl;
+		}
+	}
+	cout << "3.Kolon = ";
+	for (int b = 100; b >= 50; b--) {
+		if (b % 10 == 0) {
+			cout << b << " ";
+		}
+		if (b == 50) {
+			cout << endl;
+		}
+	}
+	cout << "4.Kolon = ";
+
+	int x{ 1 };
+	while (true) {
+		int y = pow(2, x);
+		if (y > 64) {
+			break;
+		}
+		cout << y << " ";
+		x++;
+	}
+	cout << endl;
+	return 0;
+}
+
