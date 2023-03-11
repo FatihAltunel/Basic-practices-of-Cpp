336732892fe2ce4a72b7c38fc9e8fdca3d5cfdea
conqueror1071 <127208356+conqueror1071@users.noreply.github.com>
2023-03-11 14:09:16 +0300

    C++ Çalışması 5.1
    5. çalışmayı sütunlar halinde yazdıran çalışma

---
+++ Çokkatlıdöngü2.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <clocale>
+#include<math.h>
+//1.Kolonda: 1'den 100'e kadar olan 15'in katları
+//2.Kolonda: 1'den 30'a kadar olan 5'in katları
+//3.Kolonda 100'den 50'ye kadar olan 10'un katları
+//4.Kolonda 2'den 64'e kadar olan 2'nin üstleri
+
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "Turkish");
+	int a{ 15 };
+	int b{ 5 };
+	int c{ 100 };
+	int d{ 2 };
+	while (a < 100) {
+		cout << a << "\t" << b << "\t" << c << "\t" << d << endl;
+		a += 15;
+		b += 5;
+		c -= 10;
+		d *= 2;
+	}
+
+
+}
+
