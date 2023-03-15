e8d337aee88a041fff784f0944a2cd4d4ba9dee2
conqueror1071 <127208356+conqueror1071@users.noreply.github.com>
2023-03-15 13:33:15 +0300

    C++ Çalışması 8
    Kullanıcının istediği sayıda sayı ve sütunda çarpım tablosu oluşturup ekrana yazdıran proje

---
+++ ÇarpımTablosu.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <clocale>
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "Turkish");
+	int sutun, satir;
+	cout << "Lütfen istediğiniz çarpım tablosunun sütun sayısını giriniz: ";
+	cin >> sutun;
+	cout << "Lütfen istediğiniz çarpım tablosunun satır sayısını giriniz: ";
+	cin >> satir;
+	for (int a = 1; a <= sutun; a++) {
+		for (int b = 1; b <= satir; b++) {
+			cout << a * b<<"\t";
+		}
+		cout << endl;
+	}
+
+
+
+}
