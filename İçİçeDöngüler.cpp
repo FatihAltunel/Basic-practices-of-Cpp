c37a9e0039f43f463d6bd7944d94dd8599752fd0
conqueror1071 <127208356+conqueror1071@users.noreply.github.com>
2023-03-15 13:31:49 +0300

    C++ Çalışması 8
    Kullanıcının istediği satır ve sütun sayısıyla çarpım tablosu çıkarıp ekrana yazdıran çalışma

---
+++ İçİçeDöngüler.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <clocale>
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "Turkish");
+
+	for (int x = 0; x < 10; x++) {
+		for (int y = 0; y < 10; y++) {
+			for (int z = 0; z < 10; z++) {
+				cout << x << " " << y << " " << z<<" ";
+				cout << endl;
+			}
+		}
+		cout << endl; 
+	}
+}
