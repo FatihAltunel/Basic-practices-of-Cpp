25f32cf522736151c4a03babfaff8ebc4659aece
conqueror1071 <127208356+conqueror1071@users.noreply.github.com>
2023-03-16 18:25:20 +0300

    C++ Çalışması 9
    İstenilen boyutta 0 ve 1'lerden oluşan kare bir zemin oluşturup köşegenlerini 1 olacak şekilde ekrana yazdıran çalışma

---
+++ Matris.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <clocale>
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "Turkish");
+	while(true){
+	int boyut;
+	cout << "Matriksin boyutunu giriniz: ";
+	cin >> boyut;
+
+	for (int a = boyut; a >= 1; a--) {
+		for (int dongu = 1; dongu <=boyut; dongu++) {
+			if (dongu == a||dongu==boyut-a+1) {
+				cout << "1";
+			}
+			else {
+				cout << "0";
+			}
+		}
+		cout << endl;
+	}
+	}
+}
