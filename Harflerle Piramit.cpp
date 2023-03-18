94d5c7eeeb13489806d967d02ae62471940d69fc
conqueror1071 <127208356+conqueror1071@users.noreply.github.com>
2023-03-18 20:19:39 +0300

    C++ Çalışması 11
    Kullanıcının girdiği harflerle  2D zeminde piramit oluşturan C++ Çalışması

---
+++ Harflerle Piramit.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <clocale>
+using namespace std;
+int main() {
+	while (true) {
+		setlocale(LC_ALL, "Turkish");
+		char karakter;
+		cout << "Lütfen büyük harf giriniz (Türkçe karakterler dahil değildir) :";
+		cin >> karakter;
+		for (char i = 65; i <= karakter; i++) {
+			for (char j = 65; j <= i; j++) {
+				cout << j;
+			}
+			for (char h = i - 1; h >= 65; h--) {
+				cout << h;
+			}
+			cout << endl;
+		}
+	}
+}
