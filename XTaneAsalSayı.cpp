2545c94a4898e0c927ac301940d6e58fe7df0d83
conqueror1071 <127208356+conqueror1071@users.noreply.github.com>
2023-03-22 19:03:33 +0300

    C++ Çalışması 12.2
    İlk x tane asal sayıyı ekrana yazdıran c++ çalışması

---
+++ XTaneAsalSayı.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+
+using namespace std;
+
+bool asalmi(int x) {
+	for (int i = 2; i < x; i++) {
+		if (x % i == 0 ) {
+			return false; }
+	}
+	return true;
+}
+int main() {
+	setlocale(LC_ALL, "Turkish"); 
+	int c = 1;
+	int d;
+	cout << "Bu program ilk x tane asal sayıyı sırasıyla ekrana yazdırır." << endl;
+	cout << "Lütfen bir sayı giriniz:";
+	cin >> d;  
+	cout << endl; 
+	for (int a = 1; c <= d; a++) { 
+		if (asalmi(a) == true) {
+			cout<<c<<".asal sayı --> "<<"\t" << a << endl;
+			c++;
+		}
+	}
+
+}
