90a7fbcc6887409ebc9ef71abef6a5f2f6792097
conqueror1071 <127208356+conqueror1071@users.noreply.github.com>
2023-03-22 16:30:38 +0300

    C++ Çalışması 12
    Mersenne sayılarını istenilen adıma kadar hesaplayabilen C++ çalışması.

---
+++ Mersenne Sayıları.cpp
@@ -0,0 +1,22 @@
+﻿/*2 üzeri n -1 */
+#include <iostream>
+#include<math.h>
+
+using namespace std;
+
+int mersenne(int n) {
+	int a = pow(2, n) - 1;
+	return a; 
+}
+int main() {
+	while (true) {
+		setlocale(LC_ALL, "Turkish");
+		cout << "Mersenne sayısı = 2^n -1\nTerim sayısını giriniz: ";
+		int adım;
+		cin >> adım;
+		for (int i = 0; i < adım; i++) {
+			cout << i + 1 << ". terim -> " << mersenne(i) << endl;
+		}
+		cout << endl;
+	}
+}
