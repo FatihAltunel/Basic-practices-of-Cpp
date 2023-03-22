f968cb8b7b8e916381838eca9eca508a919c81b5
conqueror1071 <127208356+conqueror1071@users.noreply.github.com>
2023-03-22 17:25:48 +0300

    C++ Çalışması 12
    Girilen sayıya kadar olan tüm asal sayıları sırasıyla ekrana yazdıran çalışma

---
+++ XSayıyaKadarAsalSayı.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int asal(int a) {
+
+	int kob{ 0 };
+	for (int b = 1; b <= a; b++) {
+		if (a % b == 0) {
+			kob++;
+		}
+	}
+	if (kob <= 2) {
+		cout << a << endl;
+	}
+	return 0;
+}
+
+int main() {
+	setlocale(LC_ALL, "Turkish"); 
+	cout << "Bu program girdiğiniz sayıya kadar olan asal sayıları sırasıyla ekrana yazdırır" << endl;
+	cout << "Lütfen bir sayı giriniz: ";
+	int sayı; 
+	cin >> sayı;
+	for (int i = 1; i <= sayı; i++) {
+		asal(i);
+	}
+
+}
