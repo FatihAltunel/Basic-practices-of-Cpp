8b4325543924003cbb00a23435c63f1bac6e0650
conqueror1071 <127208356+conqueror1071@users.noreply.github.com>
2023-03-23 21:43:09 +0300

    C++ Çalışması 14
    Kullanıcının istediği kadar terime sahip bir dizi oluşturup bu dizinin en büyük, en küçük terimlerini, terimler toplamını ve ortalamasını bularak ekrana yazdıran C++ çalışması.

---
+++ Dizilier.cpp
@@ -0,0 +1,39 @@
+﻿#include<iostream>
+using namespace std;
+int main() {
+	setlocale(LC_ALL, "Turkish");
+	cout << "Diziniz kaç terimli olsun: ";
+	int terimsayisi;
+	cin >> terimsayisi;
+	int* dizi = new int[terimsayisi];
+	for (int i = 0; i < terimsayisi; i++) {
+		cout <<"Lütfen " << i + 1 << ". terimi giriniz: ";
+		cin >> dizi[i];
+	}
+	cout << "Diziniz = [";
+	for (int i = 0; i < terimsayisi; i++) {
+		if (i < terimsayisi-1) {
+			cout<< dizi[i]<<", ";
+		}
+		else if (i = terimsayisi - 1) {
+			cout << dizi[i]<<"]" << endl;
+		}
+	}
+	int enbuyuk = dizi[0];
+	int enkucuk = dizi[0];
+	for (int i = 0; i < terimsayisi; i++) { 
+		if (dizi[i] > dizi[0]) {
+			enbuyuk = dizi[i];
+		}
+		if (dizi[i] < dizi[0])
+			enkucuk = dizi[i];
+	}
+	cout << "Dizinin en büyük sayısı = " << enbuyuk << "\nDizinin en küçük sayısı = " << enkucuk << endl;
+	int toplam{ 0 };
+	for (int i = 0; i < terimsayisi; i++) {
+		toplam += dizi[i];
+	}
+	cout << "Dizideki terimlerin toplamı = " << toplam << "\nDizideki terimlerin ortalaması = " << toplam / terimsayisi << endl; 
+	delete[]dizi; 
+	return 0;
+}
