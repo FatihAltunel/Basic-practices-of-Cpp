f97073b7071ab7b0a3e8b8da6c3acdc4f2903270
conqueror1071 <127208356+conqueror1071@users.noreply.github.com>
2023-03-23 19:23:27 +0300

    C++ Çalışması 14
    Rastgele bir zar sayısı ekrana yazdıran C++ çalışması.

---
+++ RandomNumberGenerator.cpp
@@ -0,0 +1,12 @@
+﻿#include<iostream>
+#include<limits>
+using namespace std;
+int main() {
+	while (true) {
+		setlocale(LC_ALL, "Turkish");
+		cout << "Bir zar atmak için Enter'a bas ";
+		cin.ignore(numeric_limits<streamsize>::max(), '\n');
+		int RandomNumber;
+		srand(time(NULL));
+	}
+}
