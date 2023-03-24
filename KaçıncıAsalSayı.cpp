07637b4cf75ea37122f1df36174a9f6a0a490563
conqueror1071 <127208356+conqueror1071@users.noreply.github.com>
2023-03-25 00:16:14 +0300

    C++ Çalışması 12.3
    İstenilen sıradaki asal sayıyı bulan C++ çalışması

---
+++ KaçıncıAsalSayı.cpp
@@ -0,0 +1,36 @@
+﻿#include<iostream>
+
+using namespace std;
+bool asalmi(int y) {
+	if (y == 1) {
+		return false;
+	}
+	for (int i = 2; i < y; i++) {
+		if (y % i == 0) {
+			return false;
+		}
+	}
+	return true;
+}
+
+int asal(int x) {
+	int sayac{ 0 };
+	int i;
+	for (i = 1; sayac < x; i++) {
+		if (asalmi(i)) {
+			sayac++;
+		}
+	}
+	cout <<x<<". asal sayı = " << i - 1 << endl;
+	return i-1;
+}
+
+int main() {
+	while (true) {
+		setlocale(LC_ALL, "Turkish");
+		cout << "Kaçıncı asal sayıyı bulmak istiyorsunuz: ";
+		int sayi;
+		cin >> sayi;
+		asal(sayi); 
+	}
+}
