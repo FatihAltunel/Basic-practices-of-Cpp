9855c656a1a6e8b0c58d072ee18a993e732e6ad7
conqueror1071 <127208356+conqueror1071@users.noreply.github.com>
2023-03-25 00:23:31 +0300

    C++ Çalışması 14.4
    Asal ve aynı zamanda Mersenne sayısı olan sayıları sırasıyla yazdıran C++ çalışması.

---
+++ AsalVEMersenne.cpp
@@ -0,0 +1,29 @@
+﻿#include<iostream>
+#include<math.h>
+using namespace std;
+
+bool asalmi(int y) { 
+	if (y == 1) {
+		return false;
+	}
+	for (int i = 2; i < y; i++) { 
+		if (y % i == 0) { 
+			return false;
+		}
+	}
+	return true;
+}
+int main() {
+	setlocale(LC_ALL, "Turkish"); 
+	cout << "Asal ve Mersenne sayısı olan ilk kaç eleman gösterilsin: ";
+	int sayac{ 0 };
+	int girilen; 
+	cin>>girilen;
+	for (int i = 1; sayac < girilen; i++) {
+		double islem = pow(2, i) - 1;
+		if (asalmi(islem)) { 
+			sayac++;
+			cout<<sayac<<". terim --> " << islem << endl;
+		}
+	}
+} 
