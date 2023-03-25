a7b06669e0dcac0f0867d291abdff75fc29b5cd1
conqueror1071 <127208356+conqueror1071@users.noreply.github.com>
2023-03-25 16:15:29 +0300

    C++ Çalışması 15.1
    Dizi ile ilgili birçok yeni hesaplamaları getirildi.

--- DiziElemanlarıSıralaması.cpp
+++ DiziElemanlarıSıralaması.cpp
@@ -43,5 +43,31 @@ int main() {
 		cout << dizi[i] << " ";
 	}
 	cout << endl;
+	int toplam = 0;
+	int carpim = 1;
+	int teksayilartoplami = 0; 
+	int teksayilar = 0;
+	int enbuyukcift = dizi[4]; 
+	int enkucukcift = dizi[0];
+	for (int i = 0; i < terimsayisi; i++) {
+		toplam += dizi[i];
+		if (dizi[i] % 2 == 1) {
+			teksayilartoplami += dizi[i];
+			teksayilar++;
+		}
+		carpim *= dizi[i];
+		if (dizi[i] % 2 == 0 && enbuyukcift < dizi[i]) {
+			enbuyukcift = dizi[i]; 
+		}
+		if (dizi[i] % 2 == 0 && enkucukcift > dizi[i]) {
+			enkucukcift = dizi[i];
+		}
+	}
+	cout << "Dizinin elemanlarının aritmetik ortalaması = " << (float)toplam/terimsayisi << endl; 
+	cout << "Dizinin elemanlarının geometrik ortalaması = " << pow(carpim, (float)1 / terimsayisi) << endl; 
+	cout << "Dizinin tek sayı elemanlarının aritmetik ortalaması = " << (float)teksayilartoplami / teksayilar << endl; 
+	cout << "Dizideki çift sayıların en büyüğü = " << enbuyukcift << endl; 
+	cout << "Dizideki çift sayıların en küçüğü = " << enkucukcift << endl; 
+	cout << "Dizideki çift sayıların en büyüğü ve en küçüğünün ortalaması = " << (float)(enbuyukcift + enkucukcift) / 2 <<
 	return 0;
 }
