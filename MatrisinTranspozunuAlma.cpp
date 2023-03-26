9510cd7c473600ebc6cc3a8b95fc2805ad47d639
conqueror1071 <127208356+conqueror1071@users.noreply.github.com>
2023-03-26 15:20:23 +0300

    C++
    Bir matrisin transpozunu alan c++ projesi

---
+++ MatrisinTranspozunuAlma.cpp
@@ -0,0 +1,29 @@
+﻿#include<iostream>
+using namespace std;
+int main() {
+	int dizi[3][3] = { 5,4,3,4,0,4,7,10,3 };
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++) {
+			cout << dizi[i][j] << "\t";
+		}
+		cout << endl;
+	}
+	cout << endl;	
+	cout << endl;	
+	cout << endl;	
+	cout << endl;	 
+	for (int i = 0; i < 3; i++) {
+		for (int j = i+1; j < 3; j++) { 
+			int temp;
+			temp = dizi[i][j];
+			dizi[i][j] = dizi[j][i];
+			dizi[j][i] = temp;
+		}
+	}
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++) {
+			cout << dizi[i][j] << "\t";
+		}
+		cout << endl;
+	}
+}
