be3c898ff691c2fd715b0190d3f1b1d460630e41
conqueror1071 <127208356+conqueror1071@users.noreply.github.com>
2023-03-28 03:38:19 +0300

    C++ Çalıması 18
    Kullanıcının girdiği bir dizginin polindrome olup olmadığını kontrol eden proje

---
+++ PalindromeTest.cpp
@@ -0,0 +1,36 @@
+﻿/*Bu program kullanıcıdan bir string ifade alarak bu ifadenin palindrome olup
+olmadığını kontrol eder.
+Örn. -->	abcba polindromdur
+Örn. -->	abcbd polindrome değildir*/
+#include<iostream>
+using namespace std; 
+int main() {
+	while (true) {
+		setlocale(LC_ALL, "Turkish");
+		cout << "Lütfen bir dizgi giriniz: ";
+		char girilen[100];
+		cin >> girilen;
+		char* p;
+		char* q;
+		p = girilen;
+		q = girilen;
+		while (*q != '\0') {
+			q++;
+		}
+		q--;
+		bool polindrome = true;
+		while (q > p) { 
+			if (*p != *q) {
+				polindrome = false;
+			}
+			p++;
+			q--;
+		}
+		if (polindrome) {
+			cout << "Dizginiz bir polindrome'dur" << endl;
+		}
+		else {
+			cout << "Girilen dizgi bir polindrome değildir" << endl;
+		}
+	}
+}
